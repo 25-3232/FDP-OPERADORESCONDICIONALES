cfd14018ebb5ba7db19e79777a5708031a96ab2d
25-3232 <253232@liceouniversitario.edu.mx>
2025-09-18 19:27:21 -0700

    Add files via upload

---
+++ operadores_condicionales.c
@@ -0,0 +1,17 @@
+/*
+    * Ejemplo de uso de operadores condicionales en C
+*/
+
+#include <stdio.h>
+#include <math.h>
+int main() 
+    {
+    int numero = 5;
+    if (numero == 5){
+        printf("El número es 5\n");
+    }
+    else {
+        printf("El número no es 5\n");
+    }
+    return 0;
+    }
